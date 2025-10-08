1231a7573bb50e757f90e2cdc8dd9b394fb383ed
Fakhrudeen1977 <fakhrudeen77@gmail.com>
2025-10-08 21:51:22 +0530

    War File created

--- JenkinTest/src/main/java/com/moh/gov/JenkinTestApplication.java
+++ JenkinTest/src/main/java/com/moh/gov/JenkinTestApplication.java
@@ -2,12 +2,19 @@ package com.moh.gov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinTestApplication {
+public class JenkinTestApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinTestApplication.class, args);
 	}
 
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenkinTestApplication.class);
+	}
 }
