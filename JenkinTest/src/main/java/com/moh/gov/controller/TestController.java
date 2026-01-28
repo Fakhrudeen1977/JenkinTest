fc9d798c8b79af85d82a7a882d559b5279fde0ec
Fakhrudeen1977 <fakhrudeen77@gmail.com>
2026-01-28 21:01:48 +0530

    Code Modified

--- JenkinTest/src/main/java/com/moh/gov/controller/TestController.java
+++ JenkinTest/src/main/java/com/moh/gov/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.moh.gov.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	
 	@GetMapping("/getName")
-	public void getName() {
-		System.out.println("Hi Fakhrudeen Ali Ahamed");
+	public ResponseEntity<String>  getName() {
+		String message="<h1>Welcome to Spring Boot</h1>";
+		return new ResponseEntity<String>(message,HttpStatus.OK);
+		
 	}
+	
+	
+	@GetMapping("/getMyName")
+	public ResponseEntity<String>  getMyName() {
+		String message="<h1>Hi Fakhrudeen Ali Ahamed</h1>";
+		return new ResponseEntity<String>(message,HttpStatus.OK);
+		
+	}
+	
+	
+	
+	
 
 }
